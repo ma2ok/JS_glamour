a3352a289c512a962d19b833fd800e91c444b6a9
Tubasa Matsuoka <workwing0283@gamil.com>
2020-04-08 11:47:36 +0900

    JSの動作確認

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-'use script';
+'use strict';
 
-console.log('Hello World');
+console.log('hello' + 'world');
+console.log(12*2);
+
+let price =150;
+
+console.log(price*150);
+
+//const定数  let 変数
+
+price = 170;
+console.log(price*150);
+
+console.log('5' + 3);
+console.log(parseInt('5', 10) + 3);
+
+const kkk =1200;
+
+console.log(kkk === 1000);
+
+
+const score = 40;
+//if (score >= 80){
+//  console.log('A');
+//}else if (score >= 60){
+//  console.log('B');
+//}else (score >= 40){
+//  console.log('C');
+//}
+
+
+score >= 80 ? console.log('A') : console.log('B')
